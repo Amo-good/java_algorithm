6c75b1182b9c61cfa3978974d6242161789d82f0
amo <amo233@qq.com>
2022-07-06 12:10:00 +0800

    查找排序数组中缺失的值

---
+++ Simple-algorithm/src/main/java/com/amo/solution/MissNumber.java
@@ -0,0 +1,21 @@
+package com.amo.solution;
+
+/**
+ * 一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0～n-1之内。
+ * 在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字。
+ * 输入: [0,1,3]
+ * 输出: 2
+ */
+public class MissNumber {
+    public int missingNumber(int[] nums) {
+        //缺失的数字等于 “右子数组的首位元素” 对应的索引；因此考虑使用二分法查找 “右子数组的首位元素” 。
+        int i = 0, j = nums.length - 1;
+        while(i <= j) {
+            int m = (i + j) / 2;
+            if(nums[m] == m) i = m + 1;
+            else j = m - 1;
+        }
+        return i;
+    }
+
+}
