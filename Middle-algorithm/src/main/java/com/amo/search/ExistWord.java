441773f9e7407680e99ba35cd0df8be4d2533fb3
amo <amo233@qq.com>
2022-07-16 20:35:16 +0800

    矩阵中的路径

---
+++ Middle-algorithm/src/main/java/com/amo/search/ExistWord.java
@@ -0,0 +1,33 @@
+package com.amo.search;
+
+/**
+ * 给定一个m x n 二维字符网格board和一个字符串单词word 。如果word 存在于网格中，返回 true ；否则，返回 false 。
+ * 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
+ * 输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+ * 输出：true
+ *。
+ */
+public class ExistWord {
+    public boolean exist(char[][] board, String word) {
+        char[] words = word.toCharArray();
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if(dfs(board, words, i, j, 0)) return true;
+            }
+        }
+        return false;
+    }
+    boolean dfs(char[][] board, char[] word, int i, int j, int k) {
+        //如果下标越界或者字符不匹配返回false
+        if(i >= board.length || i < 0 || j >= board[0].length || j < 0 || board[i][j] != word[k]) return false;
+        if(k == word.length - 1) return true;
+        //走过的字符标记为空（匹配上的元素），防止重复走(因为ABCCC这样，可能走到第四个C，然后往回走了)
+        board[i][j] = '\0';
+        //代表走的四个方向
+        boolean res = dfs(board, word, i + 1, j, k + 1) || dfs(board, word, i - 1, j, k + 1) ||
+                dfs(board, word, i, j + 1, k + 1) || dfs(board, word, i , j - 1, k + 1);
+        //还原原来的字符
+        board[i][j] = word[k];
+        return res;
+    }
+}
