3a3041066132c872e1b010d13d35368457a0f087
amo <amo233@qq.com>
2022-07-22 11:20:31 +0800

    重建二叉树

---
+++ Middle-algorithm/src/main/java/com/amo/divide/BuildTree.java
@@ -0,0 +1,32 @@
+package com.amo.divide;
+
+import java.util.HashMap;
+
+/**
+ * 输入某二叉树的前序遍历和中序遍历的结果，请构建该二叉树并返回其根节点。
+ * 假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+ * Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+ * Output: [3,9,20,null,null,15,7]
+ *
+ */
+public class BuildTree {
+    //用来保存根节点
+    int[] preorder;
+    HashMap<Integer, Integer> dic = new HashMap<>();
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        this.preorder = preorder;
+        for(int i = 0; i < inorder.length; i++)
+            dic.put(inorder[i], i);
+        return recur(0, 0, inorder.length - 1);
+    }
+    TreeNode recur(int root, int left, int right) {
+        if(left > right) return null;                          // 递归终止
+        TreeNode node = new TreeNode(preorder[root]);          // 建立根节点 3
+        int i = dic.get(preorder[root]);                       // 获取根节点的下标 划分根节点、左子树、右子树
+        node.left = recur(root + 1, left, i - 1);              // 开启左子树递归
+        //root + （i - left + 1） 根节点索引+左子树长度+1
+        node.right = recur(root + i - left + 1, i + 1, right); // 开启右子树递归
+        return node;                                           // 回溯返回根节点
+    }
+
+}
---
+++ Middle-algorithm/src/main/java/com/amo/divide/TreeNode.java
@@ -0,0 +1,8 @@
+package com.amo.divide;
+
+public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+}
